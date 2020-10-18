d08f84a5d10fdd3eb4dcbf28b58d79c146ac3216
FinbertMDS <ngovanhuy.cntt3@gmail.com>
2020-10-18 20:41:30 +0900

    update

---
+++ micro/runElastic.sh
@@ -0,0 +1,4 @@
+docker run -d -it --net=micro_mynetwork --name es -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.5.1 && \
+docker run -d -it --net=micro_mynetwork --name kibanak --link es:elasticsearch -p 5601:5601 docker.elastic.co/kibana/kibana:6.5.2 && \
+docker run -d -it --net=micro_mynetwork --name logm -p 5044:5044 -vvv -e xpack.monitoring.enabled=true -e xpack.monitoring.elasticsearch.hosts=127.0.0.1:9200 docker.elastic.co/logstash/logstash:6.5.2 && \
+docker run -d -it --net=micro_mynetwork --name filebeatms -p 12201:12201 -v /tmp:/usr/share/filebeat/logs/ docker.elastic.co/beats/filebeat:6.5.4
